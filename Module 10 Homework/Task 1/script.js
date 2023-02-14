6dd18c374c6d899efa5bb83fe67992d2a8890fb2
Elena-1405 <111912799+Elena-1405@users.noreply.github.com>
2023-02-14 10:09:24 +0000

    Update script.js

--- Module 10 Homework/Task 1/script.js
+++ Module 10 Homework/Task 1/script.js
@@ -1,10 +1,3 @@
- /* Задание 1.
-
-Сверстайте кнопку, которая будет содержать в себе icon_01 
-(как в примере в последнем видео). 
-При клике на кнопку иконка должна меняться на icon_02. 
-Повторный клик меняет иконку обратно.*/
-
 const btn = document.querySelector('.js-btn');
 const icon1 = document.querySelector('.icon1');
 const icon2 = document.querySelector('.icon2');
